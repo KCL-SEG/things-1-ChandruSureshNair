b3837ca383045a7ec3c6a4900a4bd9f812b761ff
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-07 23:23:54 +0100

    Add Things model to things app

--- things/models.py
+++ things/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from sys import maxsize
+from wsgiref.validate import validator
+from django.db.models import (
+  Model,
+  CharField,
+  IntegerField
+)
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Things(Model):
+  name = CharField(max_length=30, unique=True, null=False, blank=False)
+  description = CharField(max_length=120)
+  quantity = IntegerField(validators=[
+    MinValueValidator(0), MaxValueValidator(100)
+  ])
+
+  def __str__(self) -> str:
+    return '{} (x{}): {}'.format(self.name, self.quantity, self.description)
